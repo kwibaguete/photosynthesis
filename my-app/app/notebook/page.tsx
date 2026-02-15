4d55da3145f286127ea2f6a4f183fba98d114a1d
kwibaguete <bweng87@gmail.com>
2026-02-15 15:05:59 -0500

    migrate from pages to app router

---
+++ my-app/app/notebook/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>notebook page</div>
+  )
+}
+
+export default page
---
+++ my-app/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+        <h1>welcome to photosynthesis home page!</h1>
+        <p>/notebook</p>
+    </div>
+  )
+}
