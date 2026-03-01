024f2946ad555bc0bfd458e06904c6d7070bbe68
frogmoogan <meng5@binghamton.edu>
2026-03-01 13:39:14 -0500

    made pomodoro page, make accessible from home ns

---
+++ my-app/app/pomodoro/page.tsx
@@ -0,0 +1,10 @@
+//required for page to be accessible
+
+//accessible at the link: http://localhost:3000/pomodoro
+//next challenge: linking to home page..?
+
+
+//shows a separate page, now how to link back to home page (?)
+export default function Page() {
+  return <p>This is the Pomodoro Page</p>;
+}
